d89db68310053bdfb0de85a5c062879e666d353c
AdeolaRo <char.wilsons@gmail.com>
2024-09-20 09:52:22 +0200

    favorite recipe

---
+++ src/app/pages/favorite-recipe/favorite-recipe.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-favorite-recipe',
+  standalone: true,
+  imports: [],
+  templateUrl: './favorite-recipe.component.html',
+  styleUrl: './favorite-recipe.component.css'
+})
+export class FavoriteRecipeComponent {
+
+}
